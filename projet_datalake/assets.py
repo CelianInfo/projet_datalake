44c509766287c4cabb6038faf1114ef4c5680a3f
CelianInfo <celi.toureille@gmail.com>
2025-01-07 09:40:11 +0100

    need to fix json_avis_glassdoor asset

--- projet_datalake/assets.py
+++ projet_datalake/assets.py
@@ -2,7 +2,8 @@ from dagster import asset, op, Output, OpExecutionContext
 import os
 import shutil
 from bs4 import BeautifulSoup
-# soup = BeautifulSoup(html_doc, 'html.parser')
+from collections import defaultdict
+import json
 
 @op
 def list_files_in_folder(folder_path: str) -> list[str]:
@@ -35,25 +36,101 @@ def copy_file(file_path, destination_folder):
         shutil.copy(file_path, destination_folder)
 
 @op
-def parse_html_glassdoor_avis(file_paths: list[str]) -> list[dict]:
-    """
-    Parses HTML files and extracts the desired element.
+def parse_html_glassdoor_avis(file_path:str) -> dict:
+    result = {'societe':dict(),'stats':dict(),'avis':list()}
 
-    Args:
-        file_paths: A list of paths to HTML files.
+    with open(file_path, 'r', encoding='utf-8') as html_file:
+        soup = BeautifulSoup(html_file, 'html.parser')
 
-    Returns:
-        A list of dictionaries, where each dictionary represents the extracted 
-        element from a single HTML file.
-    """
-    data = []
-    for file_path in file_paths:
-        with open(file_path, 'r', encoding='utf-8') as f:
-            soup = BeautifulSoup(f, 'html.parser')
-            # Replace this with your actual element selection logic
-            element = soup.find('h1').text 
-            data.append({'file': file_path, 'element': element})
-    return data
+        # Extraction du haut de page
+
+        result['societe']['nom'] = soup.find('div', class_='header cell info').text
+
+        compagnieHeader = soup.find('div', id='EIProductHeaders')
+
+        result['societe']['nb_avis'] = int(compagnieHeader
+                .find('a', class_='eiCell cell reviews active')
+                .find('span', class_='num h2')
+                .text
+                .strip())
+        
+        result['societe']['nb_emplois'] = int(compagnieHeader
+                .find('a', class_='eiCell cell jobs')
+                .find('span', class_='num h2')
+                .text
+                .strip())
+        
+        result['societe']['nb_salaires'] = int(compagnieHeader
+                .find('a', class_='eiCell cell salaries')
+                .find('span', class_='num h2')
+                .text
+                .strip())
+        
+        result['societe']['nb_entretiens'] = int(compagnieHeader
+                .find('a', class_='eiCell cell interviews')
+                .find('span', class_='num h2')
+                .text
+                .strip())
+        
+        result['societe']['nb_avantages'] = int(compagnieHeader
+                .find('a', class_='eiCell cell benefits')
+                .find('span', class_='num h2')
+                .text
+                .strip())
+        
+        result['societe']['nb_photos'] = int(compagnieHeader
+                .find('a', class_='eiCell cell photos')
+                .find('span', class_='num h2')
+                .text
+                .strip())
+
+        # Extraction du tableau des notations utilisateurs
+
+        statsBody = soup.find('div', class_='empStatsBody')
+
+        result['stats']['notation_employes'] = float(statsBody.find('div', class_='v2__EIReviewsRatingsStylesV2__ratingNum v2__EIReviewsRatingsStylesV2__large').text)
+        result['stats']['pourc_recommandation'] = int(statsBody.find('div', id='EmpStats_Recommend').get('data-percentage'))
+        result['stats']['pourc_approbation'] = int(statsBody.find('div', id='EmpStats_Approve').get('data-percentage'))
+        
+        fondateur = statsBody.find('div', class_='donut-text d-lg-table-cell pt-sm pt-lg-0 pl-lg-sm').find('div').text.strip()
+
+        nb_eval_fondateur_txt = statsBody.find('div', class_='numCEORatings').text
+        nb_eval_fondateur = int(''.join(c for c in nb_eval_fondateur_txt if c.isdigit()))
+
+        # Extraction des avis
+
+        employeeReviews = soup.findAll('li', class_='empReview')
+
+        reviews_data = result['avis']
+        for review in employeeReviews:
+            review_data = {}
+
+            review_data['note'] = float(review.find('span', class_='value-title').get('title'))
+            review_data['titre'] = review.find('a', class_='reviewLink').find('span').text.strip()[2:-2]
+
+            description_employe = review.find('span', class_='authorJobTitle middle reviewer').text.split('-')
+            review_data['employe_status'] = description_employe[0].strip()
+            review_data['employe_poste'] = description_employe[1].strip()
+
+
+            review_data['recommandations'] = defaultdict(list) 
+            if review.find('div', class_='row reviewBodyCell recommends'):
+                for color in ('green', 'yellow', 'red'):
+                    for element in review.find('div', class_='row reviewBodyCell recommends').findAll('i', class_=color):
+                        review_data['recommandations'][color].append(element.parent.find('span').text)
+
+            review_data['anciennete'] = review.find('p', class_='mainText').text
+
+            review_data['review_body'] = defaultdict(list) 
+            if review.find('div', class_='mt-md'):
+                for review_element in review.findAll('div', class_='mt-md'):
+                    element = review_element.findAll('p')
+                    review_data['review_body'][element[0].text] = element[1].text
+        
+            reviews_data.append(review_data)
+
+
+    return result
 
 @asset
 def html_files_to_process(context):
@@ -92,3 +169,16 @@ def processed_html_files(context, html_files_to_process):
 
     return None
 
+@asset
+def json_avis_glassdoor(context):
+    current_dir = os.path.dirname(__file__) 
+    source_folder = os.path.join(current_dir, '..','TD_DATALAKE','DATALAKE','1_LANDING_ZONE','GLASSDOOR','AVI')
+    output_folder = os.path.join(current_dir, '..','TD_DATALAKE','DATALAKE','2_CURATED_ZONE','GLASSDOOR','AVI')
+
+    file_path_list = list_files_in_folder(source_folder)
+
+    for file_path in file_path_list:
+        result = parse_html_glassdoor_avis(file_path)
+        file_name = file_path[:-5]
+        with open(f"{output_folder}/{file_name}.json", "w") as outfile:
+            json.dump(result, outfile, indent=4)
--- projet_datalake/dev.py
+++ projet_datalake/dev.py
@@ -1,61 +1,66 @@
 from bs4 import BeautifulSoup
-
-# files = os.listdir(folder_path)
+from collections import defaultdict
+from pprint import pprint
+import json
 
 file_path = "C:/Users/ctoureille/Desktop/projet_datalake/TD_DATALAKE/DATALAKE/1_LANDING_ZONE/GLASSDOOR/AVI/13546-AVIS-SOC-GLASSDOOR-E12966_P1.html"
+
+def parse_html_glassdoor_avis(file_path):
+
+result = {'societe':dict(),'stats':dict(),'avis':list()}
 
 with open(file_path, 'r', encoding='utf-8') as html_file:
     soup = BeautifulSoup(html_file, 'html.parser')
 
     # Extraction du haut de page
 
-    compagnie_name = soup.find('div', class_='header cell info').text
+    result['societe']['nom'] = soup.find('div', class_='header cell info').text
 
     compagnieHeader = soup.find('div', id='EIProductHeaders')
 
-    nb_avis = int(compagnieHeader
-               .find('a', class_='eiCell cell reviews active')
-               .find('span', class_='num h2')
-               .text
-               .strip())
+    result['societe']['nb_avis'] = int(compagnieHeader
+            .find('a', class_='eiCell cell reviews active')
+            .find('span', class_='num h2')
+            .text
+            .strip())
     
-    nb_emplois = int(compagnieHeader
-               .find('a', class_='eiCell cell jobs')
-               .find('span', class_='num h2')
-               .text
-               .strip())
+    result['societe']['nb_emplois'] = int(compagnieHeader
+            .find('a', class_='eiCell cell jobs')
+            .find('span', class_='num h2')
+            .text
+            .strip())
     
-    nb_salaires = int(compagnieHeader
-               .find('a', class_='eiCell cell salaries')
-               .find('span', class_='num h2')
-               .text
-               .strip())
+    result['societe']['nb_salaires'] = int(compagnieHeader
+            .find('a', class_='eiCell cell salaries')
+            .find('span', class_='num h2')
+            .text
+            .strip())
     
-    nb_entretiens = int(compagnieHeader
-               .find('a', class_='eiCell cell interviews')
-               .find('span', class_='num h2')
-               .text
-               .strip())
+    result['societe']['nb_entretiens'] = int(compagnieHeader
+            .find('a', class_='eiCell cell interviews')
+            .find('span', class_='num h2')
+            .text
+            .strip())
     
-    nb_avantages = int(compagnieHeader
-               .find('a', class_='eiCell cell benefits')
-               .find('span', class_='num h2')
-               .text
-               .strip())
+    result['societe']['nb_avantages'] = int(compagnieHeader
+            .find('a', class_='eiCell cell benefits')
+            .find('span', class_='num h2')
+            .text
+            .strip())
     
-    nb_photos = int(compagnieHeader
-               .find('a', class_='eiCell cell photos')
-               .find('span', class_='num h2')
-               .text
-               .strip())
+    result['societe']['nb_photos'] = int(compagnieHeader
+            .find('a', class_='eiCell cell photos')
+            .find('span', class_='num h2')
+            .text
+            .strip())
 
     # Extraction du tableau des notations utilisateurs
 
     statsBody = soup.find('div', class_='empStatsBody')
 
-    employees_notation = float(statsBody.find('div', class_='v2__EIReviewsRatingsStylesV2__ratingNum v2__EIReviewsRatingsStylesV2__large').text)
-    pourc_recommandation = int(statsBody.find('div', id='EmpStats_Recommend').get('data-percentage'))
-    pourc_approbation = int(statsBody.find('div', id='EmpStats_Approve').get('data-percentage'))
+    result['stats']['notation_employes'] = float(statsBody.find('div', class_='v2__EIReviewsRatingsStylesV2__ratingNum v2__EIReviewsRatingsStylesV2__large').text)
+    result['stats']['pourc_recommandation'] = int(statsBody.find('div', id='EmpStats_Recommend').get('data-percentage'))
+    result['stats']['pourc_approbation'] = int(statsBody.find('div', id='EmpStats_Approve').get('data-percentage'))
     
     fondateur = statsBody.find('div', class_='donut-text d-lg-table-cell pt-sm pt-lg-0 pl-lg-sm').find('div').text.strip()
 
@@ -66,41 +71,35 @@ with open(file_path, 'r', encoding='utf-8') as html_file:
 
     employeeReviews = soup.findAll('li', class_='empReview')
 
-    reviews_data = []
+    reviews_data = result['avis']
     for review in employeeReviews:
         review_data = {}
 
         review_data['note'] = float(review.find('span', class_='value-title').get('title'))
+        review_data['titre'] = review.find('a', class_='reviewLink').find('span').text.strip()[2:-2]
 
-        review_data['titre'] = (review
-                                .find('a', class_='reviewLink')
-                                .find('span')
-                                .text.strip()[2:-2])
-        
-        description_employe = review.find('span',class_='authorJobTitle middle reviewer').text.split('-')
-
+        description_employe = review.find('span', class_='authorJobTitle middle reviewer').text.split('-')
         review_data['employe_status'] = description_employe[0].strip()
         review_data['employe_poste'] = description_employe[1].strip()
 
+
+        review_data['recommandations'] = defaultdict(list) 
         if review.find('div', class_='row reviewBodyCell recommends'):
-            green_recommandations =  [green.parent for green in review.find('div', class_='row reviewBodyCell recommends').findAll('i',class_='green')]
-            yellow_recommandations =  [yellow.parent for yellow in review.find('div', class_='row reviewBodyCell recommends').findAll('i',class_='yellow')]
-            red_recommandations =  [red.parent for red in review.find('div', class_='row reviewBodyCell recommends').findAll('i',class_='red')]
-            print(green_recommandations, yellow_recommandations,  red_recommandations)
+            for color in ('green', 'yellow', 'red'):
+                for element in review.find('div', class_='row reviewBodyCell recommends').findAll('i', class_=color):
+                    review_data['recommandations'][color].append(element.parent.find('span').text)
 
-        
+        review_data['anciennete'] = review.find('p', class_='mainText').text
 
+        review_data['review_body'] = defaultdict(list) 
+        if review.find('div', class_='mt-md'):
+            for review_element in review.findAll('div', class_='mt-md'):
+                element = review_element.findAll('p')
+                review_data['review_body'][element[0].text] = element[1].text
+    
         reviews_data.append(review_data)
 
-    # print(reviews_data)
-    # print(reviews_data)
 
-    # print({
-    #     'nb_avis' : nb_avis,
-    #     'nb_emplois' : nb_emplois,
-    #     'nb_salaires' : nb_salaires,
-    #     'nb_entretiens' : nb_entretiens,
-    #     'nb_avantages' : nb_avantages,
-    #     'nb_photos' : nb_photos
-    #     })
-    
+return result
+
+print(parse_html_glassdoor_avis(file_path))
